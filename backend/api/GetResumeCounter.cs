e7dae517dc4945d5704cc94d80509820befa081d
xPALien <benvoxp@gmail.com>
2022-09-04 12:49:00 -0500

    Resume Updated

--- backend/api/GetResumeCounter.cs
+++ backend/api/GetResumeCounter.cs
@@ -14,7 +14,7 @@ namespace Company.Function
 {
     public static class GetResumeCounter
     {
-        [FunctionName("GetResumeCounter")]
+        [FunctionName("GetResumeCounter9")]
         public static HttpResponseMessage Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
             [CosmosDB(databaseName: "AzureResume", collectionName: "Counter", ConnectionStringSetting = "AzureResumeConnectionString", Id = "1", PartitionKey = "1")] Counter counter,
--- frontend/main.js
+++ frontend/main.js
@@ -1,12 +1,12 @@
 window.addEventListener('DOMContentLoaded', (event) =>{
     getVisitCount();
 })
-
-const functionApi = 'http://localhost:7071/api/GetResumeCounter';
+const functionApiUrl ='https://getresumecounter9.azurewebsites.net/api/GetResumeCounter?code=E7Ed0VBDyAN8Zo3B0GF50wNylIbrANOr3vAAnrIckweLAzFuEcO5YA=='; 
+const localfunctionApi = 'http://localhost:7071/api/GetResumeCounter';
 
 const getVisitCount = () => {
     let count = 30;
-    fetch(functionApi).then(response => {
+    fetch(functionApiUrl).then(response => {
         return response.json()
     }).then(response =>{
         console.log("Website called function API.");
